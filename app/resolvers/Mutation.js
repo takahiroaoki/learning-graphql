0db2f0eb264531fa7049471ee76c8d60acd35a34
takahiro_aoki <takahiro.aoki.0902@gmail.com>
2022-06-20 17:20:29 +0900

    done chap5

--- app/resolvers/Mutation.js
+++ app/resolvers/Mutation.js
@@ -1,13 +1,24 @@
+const fetch = require('node-fetch')
 const { authorizeWithGithub } = require("../auth")
 
 module.exports = {
-    postPhoto(parent, args) {
-        var newPhoto = {
-            id: photos.length + 1,
+    async postPhoto(parent, args, { db, currentUser }) {
+        // 1. If not authorized
+        if (!currentUser) {
+            throw new Error('only an authorized user can post a photo')
+        }
+
+        // 2. Create photo data
+        const newPhoto = {
             ...args.input,
+            userID: currentUser.githubLogin,
             created: new Date()
         }
-        photos.push(newPhoto)
+
+        // 3. Store photo data
+        const { insertedIds } = await db.collection('photos').insert(newPhoto)
+        newPhoto.id = insertedIds[0]
+
         return newPhoto
     },
     async githubAuth(parent, { code }, { db }) {
@@ -43,5 +54,28 @@ module.exports = {
             .replaceOne({ githubLogin: login }, latestUserInfo, { upsert: true })
 
         return { user: latestUserInfo, token: access_token }
+    },
+    addFakeUsers: async (root, { count }, { db }) => {
+        const randomUserApi = `https://randomuser.me/api/?results=${count}`
+        const { results } = await fetch(randomUserApi).then(res => res.json())
+        const users = results.map(r => ({
+            githubLogin: r.login.username,
+            name: `${r.name.first} ${r.name.last}`,
+            avatar: r.picture.thumbnail,
+            githubToken: r.login.sha1
+        }))
+        await db.collection('users').insert(users)
+        return users
+    },
+    fakeUserAuth: async (parent, { githubLogin }, { db }) => {
+        const user = await db.collection('users').findOne({ githubLogin })
+        if (!user) {
+            throw new Error(`Cannot find user with githubLogin: ${githubLogin}`)
+        }
+
+        return {
+            token: user.githubToken,
+            user,
+        }
     }
 }
--- app/resolvers/Type.js
+++ app/resolvers/Type.js
@@ -2,23 +2,15 @@ const { GraphQLScalarType } = require('graphql')
 
 module.exports = {
     Photo: {
-        url: parent => `https://test.com/img/${parent.id}.png`,
-        postedBy: parent => {
-            return users.find(u => u.githubLogin === parent.githubUser)
+        id: parent => parent.id || parent._id,
+        url: parent => `/img/photos/${parent._id}.png`,
+        postedBy: (parent, args, { db }) => {
+            return db.collection('users').findOne({ githubLogin: parent.userID })
         },
-        taggedUsers: parent => tags
-            .filter(tag => tag.photoID === parent.id)
-            .map(tag => tag.userID)
-            .map(userID => users.find(u => u.githubLogin === userID))
     },
     User: {
-        postedPhotos: parent => {
-            return photos.filter(p => p.githubUser === parent.githubLogin)
-        },
-        inPhotos: parent => tags
-            .filter(tag => tag.userID === parent.id)
-            .map(tag => tag.photoID)
-            .map(photoID => photos.find(p => p.id === photoID))
+        postedPhotos: () => [],
+        inPhotos: () => []
     },
     DateTime: new GraphQLScalarType({
         name: "DateTime",
